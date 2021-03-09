4349dcbf60274de4dfa01873e60b5fba89bf3be0
LuSrodri <lucassrodri08@gmail.com>
2021-03-09 17:41:16 -0300

    Primeiro commit

---
+++ src/main/java/com/LucasSantos/sbWithMongoDb/SbWithMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.LucasSantos.sbWithMongoDb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbWithMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbWithMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LucasSantos/sbWithMongoDb/SbWithMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LucasSantos.sbWithMongoDb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbWithMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
