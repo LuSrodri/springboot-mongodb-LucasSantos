6f509b03f4d904954fdf5f749a9baca501419561
LuSrodri <lucassrodri08@gmail.com>
2021-03-15 20:43:16 -0300

    Obtendo um usuário por id

---
+++ src/main/java/com/LucasSantos/sbWithMongoDb/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,10 @@
+package com.LucasSantos.sbWithMongoDb.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+}
