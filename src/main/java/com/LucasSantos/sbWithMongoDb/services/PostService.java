8da6e1d6101a4e2341884a934a3d72a02d32408a
LuSrodri <lucassrodri08@gmail.com>
2021-03-23 15:22:39 -0300

    Obtendo um post por id

---
+++ src/main/java/com/LucasSantos/sbWithMongoDb/services/PostService.java
@@ -0,0 +1,23 @@
+package com.LucasSantos.sbWithMongoDb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.LucasSantos.sbWithMongoDb.domain.Post;
+import com.LucasSantos.sbWithMongoDb.repository.PostRepository;
+import com.LucasSantos.sbWithMongoDb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
